f67d8e7274bb8e3e3218653bf625c110358aa064
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-08-01 11:45:40 -0500

    Time: 137 ms (97.7%), Space: 55.4 MB (17.51%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.js
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.js
@@ -9,13 +9,14 @@ var minimumAbsDifference = function(arr) {
     let min = Math.abs(arr[1] - arr[0]);
     for (let i = 0; i < arr.length - 1; i++) {
         let temp = Math.abs(arr[i + 1] - arr[i]);
-        min = Math.min(temp, min) 
-    }
-    for (let i = 0; i < arr.length -1; i++) {
-        let temp = Math.abs(arr[i + 1] - arr[i])
+        if (temp  < min) {
+            min = temp;
+            res = []
+        }
         if (temp === min) {
             res.push([arr[i], arr[i + 1]])
         }
     }
+
     return res;
 };
