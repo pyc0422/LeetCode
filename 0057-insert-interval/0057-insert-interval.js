7d10109313b5bd20ddfe71ab1e5f810f0c27bfc1
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-09-10 21:53:13 -0500

    Time: 56 ms (89.97%), Space: 45.2 MB (14.99%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -8,19 +8,18 @@ var insert = function(intervals, newInterval) {
     // o: return a nested array inseert new one
     // c:
     // e:
-    let res = [newInterval];
-    for (let i = 0; i < intervals.length; i++) {
-        const last = res.pop();
-        const cur = intervals[i]
-        if (last[1] < cur[0]) {
-            res.push(last);
-            res.push(cur)
-        } else if (last[0] > cur[1]) {
-            res.push(cur)
-            res.push(last)
+    let [start, end] = newInterval;
+    const left = [];
+    const right = [];
+    for (let item of intervals) {
+        if (item[0] > end) {
+            right.push(item)
+        } else if (item[1] < start) {
+            left.push(item)
         } else {
-            res.push([Math.min(last[0], cur[0]), Math.max(last[1], cur[1])])
+            start = Math.min(item[0], start);
+            end = Math.max(item[1], end);
         }
     }
-    return res
-};
+    return [...left, [start, end], ...right]
+}
