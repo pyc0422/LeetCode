3d4f89812a7088f226a88111c2a96ee8ccb082f4
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-09-13 11:16:46 -0500

    Time: 70 ms (55.9%), Space: 48.2 MB (28.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -8,25 +8,22 @@ var combinationSum = function(candidates, target) {
     if (candidates.length === 1 && candidates[0] > target) {
         return []
     }
-    let temp = []
-    let helper = (index, total) => {
+    let helper = (index, total, temp) => {
         if (total > target) {
             return
         }
         if (total === target) {
-            res.push([...temp])
+            res.push(temp)
             return
         }
         for (let i = index; i < candidates.length; i++) {
             if (candidates[i] <= target) {
-                temp.push(candidates[i])
-                helper(i, total + candidates[i])
-                temp.pop()
+                helper(i, total + candidates[i], [...temp, candidates[i]])
             }
 
         }
     }
-    helper(0,0)
+    helper(0,0, [])
     return res;
     
 };
