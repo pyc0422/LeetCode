896fbc0f4b3cdf3445a90ba7f453251a770b4ae9
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-09-19 11:46:00 -0500

    Time: 103 ms (33.84%), Space: 55.9 MB (33.32%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let map = {}
+    for (let i = 0; i < nums.length; i ++) {
+        if (map[nums[i]] !== undefined) {
+            return nums[i]
+        }
+        map[nums[i]] = i
+    }
+
+};
