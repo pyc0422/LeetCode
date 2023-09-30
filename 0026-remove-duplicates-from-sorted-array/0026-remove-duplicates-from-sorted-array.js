96dbe99949974825b2115569e1f1b74f7a1c3950
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-09-29 21:49:33 -0500

    Time: 56 ms (87.13%), Space: 44.3 MB (80.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,20 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-      let i = 1;
-      let count = 1
-      while (i < nums.length && nums[i] !== '_') {
-          const cur = nums[i];
-          if (nums[i] === nums[i - 1]) {
-              nums.splice(i, 1);
-              nums.push('_');
-           
-          } else {
-              count ++;
-              i ++;
-          }
-          
-      }
-
-      return count;
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i]
+            k ++;
+        }
+    }
+    return k
 };
