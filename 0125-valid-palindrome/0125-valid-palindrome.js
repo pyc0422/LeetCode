95b0b1510e941829708208628e532462eb1747a5
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-10-06 10:38:58 -0500

    Time: 63 ms (59.58%), Space: 44.7 MB (70.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,20 +3,15 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    //i:string
-    //o:boolean
-    //c: change to lowercase,remove all the non-alphanumeric
-    //e: s.length = 0
-  let newStr = s.replace(/[^a-z0-9]/gi,"").toLowerCase();
-  let left = 0
-  let right = newStr.length - 1
-  while (left <= right){
-     if (newStr[left] !== newStr[right]) {
-    	return false 
+    s = s.replace(/[^a-z0-9]/ig,"").toLowerCase();
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false
+        }
+        left ++
+        right --
     }
-    left ++
-    right --
-  }
-  return true
-
+    return true
 };
