dfcd29c82e9e59ebd4d731a5284b9940257e68ce
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-10-06 10:41:11 -0500

    Time: 74 ms (52.97%), Space: 44.1 MB (62.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,17 +4,19 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    //i: 2 strings
-    //o: boolean,
-    //c: lowercase
-    //e: 
-    if (s.length !== t.length) {
+    if(s.length !== t.length) {
         return false
     }
-    s = s.split("").sort()
-    t = t.split("").sort()
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] !== t[i]) {
+    let map = {}
+    for (let c of s) {
+        map[c] = (map[c] || 0) + 1
+    }
+    for (let c of t) {
+        if (!map[c]) {
+            return false
+        }
+        map[c] --
+        if (map[c] < 0) {
             return false
         }
     }
