2882d254a8d91696727f0a6aa1ea8f68ea38d474
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-10-09 22:52:05 -0500

    Time: 60 ms (45.5%), Space: 44.7 MB (13.13%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -8,22 +8,23 @@ var longestPalindrome = function(s) {
     //c: case sensitive
     //e: !s.length
     // abccbefg => bcacb 5
-    if (s === s.split("").reverse().join("")) {
-        return s.length
-    }
+    let res = 0
     let map = {}
     for (let c of s) {
         map[c] = (map[c] || 0) + 1
+        if (map[c] % 2 === 0) {
+            res += 2
+        }
     }
     //map = {a:1, b:1, c:4, d:2}
-    let res = 0
-    for (let k in map) {
-        if (map[k] % 2 === 0) {
-            res += map[k]
-        } else if (map[k] > 1) {
-            res += (map[k] - 1)
-        } 
-    }
+    // let res = 0
+    // for (let k in map) {
+    //     if (map[k] % 2 === 0) {
+    //         res += map[k]
+    //     } else if (map[k] > 1) {
+    //         res += (map[k] - 1)
+    //     } 
+    // }
     return s.length > res ? res += 1 : res
 
 };
