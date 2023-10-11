1ba72cda2768956633a434268e6126ef6b0fc725
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-10-10 22:03:38 -0500

    Time: 519 ms (5.34%), Space: 84.3 MB (5%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,21 +3,41 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let left = 0;
-    let right = 1;
-    if (s.length === 0) {
+ //i：string with characters no numbers
+ //o: number present the length of the longest subsring
+ //c: 
+ //e: !s.length
+ //"abbcd" -> bcd => 3
+    if (!s.length) {
+        console.log(s)
         return 0
     }
-    let res = 1
+    if (s.length === 1) {
+        return 1
+    }
+    let left = 0
+    let right = 1
+    let map = {}
+    let res = 0
+    let temp = 0
+
     while (right < s.length) {
-        let temp = s.slice(left, right + 1)
-        if (new Set(temp).size === temp.length) {
-            res = Math.max(res, temp.length)
+        if (map[s[left]] === undefined) {
+            map[s[left]] = 1
+            temp += 1   
+        }
+        if (map[s[right]] === undefined) {
+            map[s[right]] = 1
+            temp += 1
             right += 1
+            res = Math.max(res, temp)
         } else {
+            map = {}
+            res = Math.max(res, temp)
+            temp = 0
             left += 1
             right = left + 1
         }
     }
-    return res;
+    return res
 };
