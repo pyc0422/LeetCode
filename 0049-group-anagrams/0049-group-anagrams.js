7521c80fda8f00da5a20fe666154ed1d89a584a6
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2023-12-05 21:32:47 -0600

    Time: 108 ms (63.97%), Space: 54.9 MB (23.96%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,24 +3,13 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    // i: array of strs
-    // o: nested array, inner array should be all of anagrams
-    // c: lower case or uppercase? length?
-    // e: only one elem in the input or empty array
-    // ['abc', 'cba', 'catt', 'bca', 'ctat']
-    // [['abc','cba','bca'], ['catt', 'ctat]]
-    // variable store a map of different anagram
-    let map = {}
-    //iterate the strs
-    for (let str of strs) {
-        //sort each str in same method
-        const cur = str.split("").sort().join("")
-        //check if the sorted string is already exists in the map or not
-        if (map[cur] === undefined) {
-            map[cur] = []
+    let tracker = {};
+    for (let n of strs) {
+        const temp = n.split("").sort().join("");
+        if (tracker[temp] === undefined) {
+            tracker[temp] = []
         }
-        map[cur].push(str)
+        tracker[temp].push(n)
     }
-    // get the values list of the map 
-    return Object.values(map)
+    return Object.values(tracker)
 };
