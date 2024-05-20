3eb8c722c34207aa9be8f55d9532dfef066ec034
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2024-05-20 15:04:59 -0500

    Time: 85 ms (6.51%), Space: 52.9 MB (94.57%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {TreeNode}
+ */
+var removeLeafNodes = function(root, target) {
+  
+
+    if (root.left) {
+        root.left = removeLeafNodes(root.left, target)
+    } 
+    if (root.right) {
+        root.right = removeLeafNodes(root.right, target)
+    }
+  
+    if (!root.right && !root.left && root.val === target) {
+        return null
+    }
+    return root
+};
