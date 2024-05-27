0dfdca883d61e90bdf082abc7cef1f8ac0873bf7
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2024-05-26 21:14:01 -0500

    Time: 417 ms (5.02%), Space: 59.5 MB (5.10%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    // i: array of 0,1 which 0,1 not next to each other, int n
+    // o: boolean indicate if the n flower can be planted under the rule
+    // c: array length >= 1
+    // e: n == 0
+    if (n === 0) return true;
+    if (n === 1 && flowerbed.length === 1) return flowerbed[0] === 0
+    // if (n > Math.floor(flowerbed.length / 2)) return false
+    let i = 0;
+    
+    while (i < flowerbed.length) {
+        const left = i - 1 
+        const right = i + 1
+        // console.log('i', i)
+        if (flowerbed[i] === 0) {
+            // console.log('i === 0', flowerbed)
+            // console.log('left,', left, flowerbed[left])
+            // console.log('right', right, flowerbed[right])
+            if (left < 0 && flowerbed[right] === 0) {
+                console.log('in first if')
+                flowerbed[i] = 1
+                n --;
+            }
+            if (right > flowerbed.length - 1 && flowerbed[left] === 0) {
+                console.log('in second if')
+                flowerbed[i] = 1
+                n--;
+            } 
+            if (flowerbed[left] === 0 && flowerbed[right] ===0) {
+                flowerbed[i] = 1;
+                console.log('changed,' , flowerbed, i)
+                n --;   
+            }
+        }
+        i ++;
+    }
+    console.log('n', n)
+    return n > 0 ? false : true
+};
