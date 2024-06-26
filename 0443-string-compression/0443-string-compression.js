b673c0205952e58b2953dbbc0653c90af57f9c34
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2024-06-26 12:18:19 -0500

    Time: 51 ms (95.46%), Space: 51.5 MB (72.72%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -14,17 +14,15 @@ var compress = function(chars) {
     
     while (right <= chars.length) {
         const count = right - left;
-        console.log('start', chars.length, left, right, count)
         if (chars[right] !== chars[left]) {
             if (count > 1) {
-                console.log('count larger')
                 const s = count.toString()
                 for (let num of s) {
                     chars[left + 1] = num;
                     left = left + 1;
                 }
                 chars.splice(left + 1, count - s.length - 1 );     
-                console.log('after:', chars, left,right, count)
+  
             } 
             left += 1;
             right = left + 1;
@@ -32,9 +30,6 @@ var compress = function(chars) {
             right ++;
         }
     }
-    console.log( right - left)
 
-    // left ++;
-    console.log(chars )
-    return left+1;
+    return chars.length;
 }
