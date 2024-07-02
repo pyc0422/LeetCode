2e7831376204315f761a8f185deeaf466418b0f8
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2024-07-02 13:30:11 -0500

    Time: 86 ms (51.85%), Space: 56.6 MB (31.59%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -6,17 +6,11 @@
 var findDifference = function(nums1, nums2) {
     nums1 = new Set(nums1);
     nums2 = new Set(nums2);
-    let res = [];
     for (let n of nums1) {
         if (nums2.has(n)) {
             nums2.delete(n)
             nums1.delete(n)
         }
     }
-    // for (let n of nums2) {
-    //     if (nums1.has(n)) {
-    //         nums1.delete(n)
-    //     }
-    // }
-    return [Array.from(nums1), Array.from(nums2)]
+    return [[...nums1], [...nums2]]
 };
