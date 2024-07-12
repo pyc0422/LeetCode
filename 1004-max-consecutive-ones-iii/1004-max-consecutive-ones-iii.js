4b989d0d2b51d55cd84f542db55bad6fe5814253
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2024-07-12 09:44:00 -0500

    Time: 61 ms (73.83%), Space: 53.7 MB (81.94%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let left = 0;
+    let right = 0;
+    while (right < nums.length) {
+        if (nums[right] === 0) {
+            k--
+        }
+        if (k < 0) {
+            if (nums[left] === 0) {
+                k ++
+            }
+            left ++
+        }
+        right ++;
+    }
+    return right - left;
+};
