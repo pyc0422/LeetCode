9534b316febc4387caabfa58cf00e2696171b179
Yuchen Pan <86500068+pyc0422@users.noreply.github.com>
2024-07-11 22:33:35 -0500

    Time: 52 ms (79.63%), Space: 51 MB (62.96%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseParentheses = function(s) {
+    let stack = [];
+    for (let c of s) {
+        if (c === '(') {
+            stack.push(c)
+        } else if (c === ')') {
+            let temp = '';
+            while (stack.length && stack[stack.length - 1] !== '(') {
+                const cur = stack.pop().split('').reverse().join('')
+                temp += cur;
+            }
+            stack.pop();
+            stack.push(temp)
+        } else {
+            stack.push(c)
+        }
+    }
+    return stack.join('')
+};
